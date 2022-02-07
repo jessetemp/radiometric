8d704ffb31b6d8f39e6bc57d5c4d9d7611fba70d
jessetemp <jesse.temp@gmail.com>
2022-02-07 12:33:35 -0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import { useRoute } from 'vue-router'
+  const year = new Date().getFullYear();
+</script>
+
+<template>
+  <router-view :key="useRoute().fullPath"></router-view>
+</template>
+
+<style>
+
+</style>
